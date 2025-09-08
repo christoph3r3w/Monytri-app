0807aa673aa12dfb2dcb6f9d64ddd6240a9427bc
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2025-09-08 22:45:09 +0200

    now using env file #7

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -1,10 +1,11 @@
 import { Client, Account, Databases } from 'appwrite';
+import { PUBLIC_APPWRITE_PROJECT_ID,PUBLIC_APPWRITE_ENDPOINT } from '$env/static/public';
 
 export const client = new Client();
 
 client
-    .setEndpoint('https://fra.cloud.appwrite.io/v1')
-    .setProject('689b931b001bb5a31e22'); // Replace with your project ID
+    .setEndpoint(PUBLIC_APPWRITE_ENDPOINT)
+    .setProject(PUBLIC_APPWRITE_PROJECT_ID); // Replace with your project ID
 
 export const account = new Account(client);
 export const databases = new Databases(client);
