d93d66a0daf2e5d7ed0beb69227041015a944eac
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2025-09-13 01:49:00 +0200

    Cors Failure Fix

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,6 +1,6 @@
 // import { dev } from '$app/environment';
-// export const ssr = false;
-// export const prerender = true
+export const ssr = false;
+export const prerender = true
 // export const ssr = dev;
 
 
