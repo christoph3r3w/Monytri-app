bd1e6df9de998c56aee9157f8a4404e546019a9d
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2025-09-18 23:11:51 +0200

    Preparations for authentication refactoring
    - Adjusted the Appwrite and proxy servers to accept the new domain name.
    - Moved some logic server-side
    - Working out some authentication method in the layout server side.

---
+++ src/lib/server/appwrite.js
@@ -0,0 +1,12 @@
+import { Client, Account, Databases } from 'appwrite';
+import { PUBLIC_APPWRITE_PROJECT_ID,PUBLIC_APPWRITE_ENDPOINT } from '$env/static/public';
+
+export const client = new Client();
+
+client
+    .setEndpoint(PUBLIC_APPWRITE_ENDPOINT)
+    .setProject(PUBLIC_APPWRITE_PROJECT_ID); // Replace with your project ID
+
+export const account = new Account(client);
+export const databases = new Databases(client);
+export { ID } from 'appwrite';
