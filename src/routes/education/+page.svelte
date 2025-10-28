ed57a68fc14965b49146e348621e18d3b19f4416
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2025-10-28 14:48:52 +0100

    Dynamic heights education page

--- src/routes/education/+page.svelte
+++ src/routes/education/+page.svelte
@@ -47,11 +47,21 @@
 		subgrid 
 		[left-start] repeat(5,[mid-left]) [left-end right-start] repeat(5,[mid-right]) [right-end];
 		grid-template-rows: minmax(min-content,4px) 1fr 1fr 3fr;
-		height: calc(100cqh - var(--header-height) + var(--progressbar-height) + 10px);
+		height: calc(100cqh - var(--header-height) + 10px);
 		max-height: calc(100dvh - var(--footer-height) + var(--progressbar-height)); 
 		background-color: var(--general-background-color);
 
 		container-type:normal;
 		container-name:edu-articles;
 	}
+
+	@media (-webkit-min-device-pixel-ratio: 3), 
+screen and (device-width < 900px) and (width <= 900px) and (orientation: portrait), 
+screen and (device-height <= 900px) and (height <= 900px) and (orientation: landscape) {
+
+		:global(.edu-container > .page-container){
+			height : calc(100cqh - var(--header-height) - var(--footer-height) - var(--progressbar-height) - 20px);
+		}
+	}
+
 </style>
