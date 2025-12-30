15626fabcb6ccf406f9ba6793a172a45026a5d6a
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2025-12-30 03:45:45 +0100

    general updates

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+// import tailwindcss from 'tailwindcss/vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+	sveltekit(),
+	],
 	server: {
 		port: 3003,
 	  },
